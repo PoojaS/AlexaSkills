ed64539c6d1c2cbea686ae3c710d50af05b0640f
Pooja Subramanian <Pooja.Subramanian@Honeywell.com>
2017-11-26 14:26:01 +0530

    Add event handler for intent request

--- hello-world/src/main/java/com/alexa/helloworld/HelloWorldSpeechlet.java
+++ hello-world/src/main/java/com/alexa/helloworld/HelloWorldSpeechlet.java
@@ -1,12 +1,14 @@
 package com.alexa.helloworld;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.SessionEndedRequest;
 import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.speechlet.SpeechletV2;
+import com.amazon.speech.ui.Card;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
@@ -20,9 +22,34 @@ public class HelloWorldSpeechlet implements SpeechletV2 {
     @Override
     public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
         String speechText = "Welcome to the Alexa Skills, you can say hello";
-        return SpeechletResponse.newAskResponse(getSpeech(speechText),
-                              getReprompt(getSpeech(speechText)),
-                              getSimpleCard(speechText));
+        PlainTextOutputSpeech speech = getSpeech(speechText);
+        return SpeechletResponse.newAskResponse(speech,
+                getReprompt(speech),
+                getSimpleCard(speechText));
+    }
+
+    @Override
+    public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
+        String speechText;
+        Intent intent = requestEnvelope.getRequest().getIntent();
+        String intentName = intent != null ? intent.getName() : null;
+        if ("HelloWorldIntent".equals(intentName)) {
+            speechText = "Hello there user!";
+            return SpeechletResponse.newTellResponse(getSpeech(speechText), getSimpleCard(speechText));
+        } else if ("HelpIntent".equals(intentName)) {
+            speechText = "You can say hello to me";
+            PlainTextOutputSpeech speech = getSpeech(speechText);
+            return SpeechletResponse.newAskResponse(speech, getReprompt(speech), getSimpleCard(speechText));
+        } else {
+            speechText = "Unable to recognise what was said. Please ask again.";
+            PlainTextOutputSpeech speech = getSpeech(speechText);
+            return SpeechletResponse.newAskResponse(speech, getReprompt(speech), getSimpleCard(speechText));
+        }
+    }
+
+    @Override
+    public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
+
     }
 
     private SimpleCard getSimpleCard(String speechText) {
@@ -42,15 +69,5 @@ public class HelloWorldSpeechlet implements SpeechletV2 {
         PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
         speech.setText(speechText);
         return speech;
-    }
-
-    @Override
-    public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
-        return null;
-    }
-
-    @Override
-    public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
-
     }
 }
--- hello-world/src/test/java/com/alexa/helloworld/HelloWorldSpeechletTest.java
+++ hello-world/src/test/java/com/alexa/helloworld/HelloWorldSpeechletTest.java
@@ -1,6 +1,8 @@
 package com.alexa.helloworld;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
@@ -8,15 +10,23 @@ import com.amazon.speech.ui.SimpleCard;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HelloWorldSpeechletTest {
 
     private HelloWorldSpeechlet helloWorldSpeechlet;
-    private SpeechletRequestEnvelope<LaunchRequest> requestEnvelope;
+
+    @Mock
+    private SpeechletRequestEnvelope<LaunchRequest> launchRequestEnvelope;
+
+    @Mock
+    private SpeechletRequestEnvelope<IntentRequest> intentRequestEnvelope;
 
     @Before
     public void setUp() throws Exception {
@@ -25,7 +35,7 @@ public class HelloWorldSpeechletTest {
 
     @Test
     public void shouldReturnASimpleCardOnLaunchOfSkill() throws Exception {
-        SpeechletResponse speechletResponse = helloWorldSpeechlet.onLaunch(requestEnvelope);
+        SpeechletResponse speechletResponse = helloWorldSpeechlet.onLaunch(launchRequestEnvelope);
 
         SimpleCard responseCard = (SimpleCard) speechletResponse.getCard();
         assertThat(responseCard.getTitle()).isEqualTo("Hello World");
@@ -36,5 +46,50 @@ public class HelloWorldSpeechletTest {
 
         PlainTextOutputSpeech responseSpeech = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
         assertThat(responseSpeech.getText()).isEqualTo("Welcome to the Alexa Skills, you can say hello");
+    }
+
+    @Test
+    public void shouldReturnASimpleCardOnHelloWorldIntentOfSkill() throws Exception {
+        IntentRequest intentRequestMock = mock(IntentRequest.class);
+        when(intentRequestEnvelope.getRequest()).thenReturn(intentRequestMock);
+        when(intentRequestMock.getIntent()).thenReturn(new Intent.Builder().withName("HelloWorldIntent").build());
+        SpeechletResponse speechletResponse = helloWorldSpeechlet.onIntent(intentRequestEnvelope);
+
+        SimpleCard responseCard = (SimpleCard) speechletResponse.getCard();
+        assertThat(responseCard.getTitle()).isEqualTo("Hello World");
+        assertThat(responseCard.getContent()).isEqualTo("Hello there user!");
+
+        PlainTextOutputSpeech responseSpeech = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
+        assertThat(responseSpeech.getText()).isEqualTo("Hello there user!");
+    }
+
+    @Test
+    public void shouldReturnASimpleCardWithHelpTextOnHelpIntentOfSkill() throws Exception {
+        IntentRequest intentRequestMock = mock(IntentRequest.class);
+        when(intentRequestEnvelope.getRequest()).thenReturn(intentRequestMock);
+        when(intentRequestMock.getIntent()).thenReturn(new Intent.Builder().withName("HelpIntent").build());
+        SpeechletResponse speechletResponse = helloWorldSpeechlet.onIntent(intentRequestEnvelope);
+
+        SimpleCard responseCard = (SimpleCard) speechletResponse.getCard();
+        assertThat(responseCard.getTitle()).isEqualTo("Hello World");
+        assertThat(responseCard.getContent()).isEqualTo("You can say hello to me");
+
+        PlainTextOutputSpeech responseSpeech = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
+        assertThat(responseSpeech.getText()).isEqualTo("You can say hello to me");
+    }
+
+    @Test
+    public void shouldReturnAUnrecognisedResponseWhenUnknownIntentOfSkillReceived() throws Exception {
+        IntentRequest intentRequestMock = mock(IntentRequest.class);
+        when(intentRequestEnvelope.getRequest()).thenReturn(intentRequestMock);
+        when(intentRequestMock.getIntent()).thenReturn(new Intent.Builder().withName("UknownIntent").build());
+        SpeechletResponse speechletResponse = helloWorldSpeechlet.onIntent(intentRequestEnvelope);
+
+        SimpleCard responseCard = (SimpleCard) speechletResponse.getCard();
+        assertThat(responseCard.getTitle()).isEqualTo("Hello World");
+        assertThat(responseCard.getContent()).isEqualTo("Unable to recognise what was said. Please ask again.");
+
+        PlainTextOutputSpeech responseSpeech = (PlainTextOutputSpeech) speechletResponse.getOutputSpeech();
+        assertThat(responseSpeech.getText()).isEqualTo("Unable to recognise what was said. Please ask again.");
     }
 }
