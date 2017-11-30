53463e44c63e9727f96bc20e4dd1e6163924b6e7
PoojaS <poojas2406@gmail.com>
2017-11-30 16:49:22 +0530

    Remove key

--- train-status/src/main/java/com/alexa/trainstatus/TrainStatusSpeechletRequestStreamHandler.java
+++ train-status/src/main/java/com/alexa/trainstatus/TrainStatusSpeechletRequestStreamHandler.java
@@ -11,7 +11,6 @@ public class TrainStatusSpeechletRequestStreamHandler extends SpeechletRequestSt
     static {
         //TODO (Pooja): relevant supported application id
         supportedApplicationIds = new HashSet<String>();
-        supportedApplicationIds.add("amzn1.ask.skill.e93c85c9-132c-4293-8ad2-7ce4d2cad428");
     }
 
     public TrainStatusSpeechletRequestStreamHandler() {
